f7cf0d7f11a4a6bf2ede9968bedab8eaf210072d
shivam shukla <shivamshukla8112005@gmail.com>
2025-07-12 16:29:07 +0530

    call login signup api

--- Skill-Swap Backend/routes/authRoutes.js
+++ Skill-Swap Backend/routes/authRoutes.js
@@ -110,6 +110,13 @@ router.post('/register', async (req, res) => {
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
+         const cookieOption = {
+                    // httpOnly: true,
+                    secure: true,
+                    sameSite: "None",
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
+                    path: "/",
+                };
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new User({ name, email, password: hashedPassword, skills });
@@ -117,8 +124,12 @@ router.post('/register', async (req, res) => {
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
             expiresIn: "2w"
         });
+         return res
+                    .status(201)
+                    .cookie("token", token, cookieOption)
+                    .json({status: 201, user:newUser,message: "registered succesfully"});
         // res.status(201).json({token,message: 'User registered successfully'});
-        res.status(201).json({ token, message: 'Register successful', user: { id: newUser._id, name: newUser.name, email: newUser.email } });
+    //   return  res.status(201).json({ token, message: 'Register successful', user: { id: newUser._id, name: newUser.name, email: newUser.email } });
     }
     catch (error) {
         res.status(500).json({ message: 'Server error' });
@@ -137,7 +148,18 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign({ id: userExist._id }, process.env.JWT_SECRET, {
         expiresIn: "2w"
     });
-    res.status(200).json({ token, message: 'Login successful', user: { id: userExist._id, name: userExist.name, email: userExist.email } });
+     const cookieOption = {
+                    // httpOnly: true,
+                    secure: true,
+                    sameSite: "None",
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
+                    path: "/",
+                };
+                return res
+                    .status(200)
+                    .cookie("token", token, cookieOption)
+                    .json({status: 200, user:userExist,message: "login succesfully"});
+//    return res.status(200).json({ token, message: 'Login successful', user: { id: userExist._id, name: userExist.name, email: userExist.email } });
 });
 //sign in with google
 export default router;
